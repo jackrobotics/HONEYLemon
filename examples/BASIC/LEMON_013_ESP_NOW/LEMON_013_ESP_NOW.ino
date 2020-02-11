b3d7af12e11e9f6f75856ceee6abebc934777384
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2020-02-12 04:25:53 +0700

    fix esp now

--- examples/BASIC/LEMON_013_ESP_NOW/LEMON_013_ESP_NOW.ino
+++ examples/BASIC/LEMON_013_ESP_NOW/LEMON_013_ESP_NOW.ino
@@ -6,7 +6,10 @@
 
 // ฟังก์ชั่นสำหรับการรับข้อมูลสำหรับ ESP-NOW
 void espnowRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-    Serial.print("["+lemon.espnow_mac2str(mac_addr)+"]:");
+    char macStr[22];
+    snprintf(macStr, sizeof(macStr), "[%02X:%02X:%02X:%02X:%02X:%02X]: ", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+
+    Serial.print(macStr);
     Serial.write(data, data_len);
     Serial.println();
 }
