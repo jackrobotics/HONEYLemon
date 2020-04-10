e919d3a309641e005251a323227ce8e1da579828
Sonthaya Boonchan <sonthaya.jackrobotic@mail.kmutt.ac.th>
2020-04-10 21:35:03 +0700

    add LEMON_003_MODBUS_TEMP_HUMI_MB1TH

---
+++ examples/ADVANCED/LEMON_003_MODBUS_TEMP_HUMI_MB1TH/LEMON_003_MODBUS_TEMP_HUMI_MB1TH.ino
@@ -0,0 +1,28 @@
+/*
+    Advanced : การเขียนโปรแกรมอ่านค่า Sensor Temp & Humi (MB1TH) ผ่าน Modbus Protocol บนบอร์ดไมโครคอนโทรลเลอร์ HONEYLemon
+*/
+#include <HONEYLemon.h>
+
+void setup()
+{
+    lemon.begin();          // เรียกใช้งานฟังก์ชั่นเริ่มต้นของบอร์ด HONEYLemon
+
+    Serial.begin(115200);   // เรียกใช้งาน Serial
+    lemon.debug(Serial);    // ขอดู debug ของบอร์ด HONEYLemon ผ่าน Serial
+
+    lemon.setModeModbusRTU();   // เปิดโหมดการใช้งาน Modbus RTU
+}
+
+unsigned long lastUpdate = 0;   // สร้างตัวแปรสำหรับการเช็คเวลาล่าสุดในการอ่านค่า Modbus
+
+void loop()
+{
+    if( millis()-lastUpdate > 1000 ){   // ทำงานทุกๆ 1000 มิลลิวินาที ( 1 วินาที )
+        lastUpdate = millis();          // Update เวลาล่าสุดเก็บไว้ใน lastUpdate
+
+        float humi = lemon.readIregModbusRTU(1,0)/10.0;   // อ่านค่าจาก id: 1, address: 0 เก็บไว้ในตัวแปร humi
+        float temp = lemon.readIregModbusRTU(1,1)/10.0;   // อ่านค่าจาก id: 1, address: 1 เก็บไว้ในตัวแปร temp
+
+        Serial.println("Temp: "+String(temp)+" ,Humi: "+String(humi));  // แสดงผลผ่านหน้าจอมอนิเตอร์
+    }
+}
